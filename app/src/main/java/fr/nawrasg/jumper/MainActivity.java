f61ce70a05557617704a36b19b60ae95e17591ca
Nawras GEORGI <support@nawrasg.fr>
2016-08-04 23:09:55 +0200

    Add menu

--- app/src/main/java/fr/nawrasg/jumper/MainActivity.java
+++ app/src/main/java/fr/nawrasg/jumper/MainActivity.java
@@ -1,7 +1,14 @@
 package fr.nawrasg.jumper;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import fr.nawrasg.jumper.activities.SettingsActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater nInflater = getMenuInflater();
+        nInflater.inflate(R.menu.activity_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.itemSettings:
+                startActivity(new Intent(this, SettingsActivity.class));
+                return true;
+        }
+        return false;
     }
 }
